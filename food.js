59e2f21c90624de424d3fdf11a26f442c7c1401a
Martin M. S. Pedersen <martin@linux.com>
2017-07-23 22:49:40 +0200

    Changed all to use prototype

--- food.js
+++ food.js
@@ -4,26 +4,24 @@ function Food(value) {
 	this.value = value;
 	this.destroy = false;
 	this.noise = 15;
-
-	this.show = function() {
-		fill(255,0,0);
-		rect(this.xpos,this.ypos,blockSize,blockSize);
-	}
-
-	this.collision = function(x,y) {
-		if ( (abs(this.xpos - x) < this.noise)  &&
-		     (abs(this.ypos - y) < this.noise)) {
-			this.destroy = true;
-			return true;
-		}
-		return false;
-	}
-
-	this.newPosition = function() {
-		this.xpos = random(3*blockSize,width-3*blockSize);
-		this.ypos = random(3*blockSize,height-3*blockSize);
-		this.value = 2;
-	}
 }
 
+Food.prototype.show = function() {
+	fill(255,0,0);
+	rect(this.xpos,this.ypos,blockSize,blockSize);
+}
 
+Food.prototype.collision = function(x,y) {
+	if ( (abs(this.xpos - x) < this.noise)  &&
+		(abs(this.ypos - y) < this.noise)) {
+		this.destroy = true;
+		return true;
+	}
+	return false;
+}
+
+Food.prototype.newPosition = function() {
+	this.xpos = random(3*blockSize,width-3*blockSize);
+	this.ypos = random(3*blockSize,height-3*blockSize);
+	this.value = 2;
+}
--- snake.js
+++ snake.js
@@ -9,115 +9,109 @@ function Snake(headX, headY, blockSize, headColour, bodyColour) {
 	this.speed = 1;
 	this.growing = 0;
 
-	var b = new Block(this.headX+this.blockSize, this.headY, this.blockSize, this.bodyColour);
-	this.body.push(b);
+	var b;
 
-	var b = new Block(this.headX+2*this.blockSize, this.headY, this.blockSize, this.bodyColour);
-	this.body.push(b);
+	for (i = 1; i <= 3 ; i++) {
+		var b = new Block(this.headX+i*this.blockSize, this.headY, this.blockSize, this.bodyColour);
+		this.body.push(b);
+	}
+}
 
-	var b = new Block(this.headX+3*this.blockSize, this.headY, this.blockSize, this.bodyColour);
-	this.body.push(b);
+Snake.prototype.show = function() {
+	noStroke();
+	fill(255)
+	rect(this.headX,this.headY,this.blockSize,this.blockSize);
+	for (var i = 0; i < this.body.length; i++) {
+		this.body[i].show();
+	}
+}
 
-	this.show = function() {
-		noStroke();
-		//fill(this.headColour);
-		fill(255)
-		rect(this.headX,this.headY,this.blockSize,this.blockSize);
-		for (var i = 0; i < this.body.length; i++) {
-			this.body[i].show();
+Snake.prototype.grow = function(v) {
+	this.growing = this.growing + v;
+}
+
+Snake.prototype.move = function() {
+	var len = this.body.length;
+	var oldPosX;
+	var oldPosY;
+	var b;
+
+	if (this.growing == 0) {
+		for (var i = 1; i < len; i = i + 1) {
+			oldPosX = this.body[i].xpos;
+			oldPosY = this.body[i].ypos;
+			this.body[i-1].newPosition(oldPosX,oldPosY);
 		}
+		this.body[len-1].newPosition(this.headX,this.headY);
+	} else {
+		b = new Block(this.headX, this.headY, this.blockSize, this.bodyColour);
+		this.body.push(b);
+		this.growing = this.growing - 1;
 	}
 
-	this.grow = function(v) {
-		this.growing = this.growing + v;
+	if (this.direction == "WEST") {
+		this.headX = this.headX - this.blockSize*this.speed;
+	}
+	if (this.direction == "NORTH") {
+		this.headY = this.headY - this.blockSize*this.speed;
+	}
+	if (this.direction == "SOUTH") {
+		this.headY = this.headY + this.blockSize*this.speed;
+	}
+	if (this.direction == "EAST") {
+		this.headX = this.headX + this.blockSize*this.speed;
+	}
+}
+
+Snake.prototype.changeDirectionLeft = function() {
+	if (this.direction == "NORTH") {
+		this.direction = "WEST";
+		return;
+	}
+	if (this.direction == "SOUTH") {
+		this.direction = "EAST";
+		return;
+	}
+	if (this.direction == "EAST") {
+		this.direction = "NORTH";
+		return;
+	}
+	if (this.direction == "WEST") {
+		this.direction = "SOUTH";
+	}
+}
+
+Snake.prototype.changeDirectionRight = function() {
+	if (this.direction == "NORTH") {
+		this.direction = "EAST";
+		return;
+	}
+	if (this.direction == "SOUTH") {
+		this.direction = "WEST";
+		return;
+	}
+	if (this.direction == "EAST") {
+		this.direction = "SOUTH";
+		return;
+	}
+	if (this.direction == "WEST") {
+		this.direction = "NORTH";
+	}
+}
+
+Snake.prototype.collision = function() {
+	var i;
+
+	if (this.headX < this.blockSize) return true;
+	if (this.headY < this.blockSize) return true;
+	if (this.headX > width - 2*this.blockSize) return true;
+	if (this.headY > height - 2*this.blockSize) return true;
+
+	for (i = 0; i < this.body.length-1; i++) {
+		if ((this.headX == this.body[i].xpos) && (this.headY == this.body[i].ypos)) return true;
 	}
 
-	this.move = function() {
-		var len = this.body.length;
-		var oldPosX;
-		var oldPosY;
-		var b;
-
-		if (this.growing == 0) {
-			for (var i = 1; i < len; i = i + 1) {
-				oldPosX = this.body[i].xpos;
-				oldPosY = this.body[i].ypos;
-				this.body[i-1].newPosition(oldPosX,oldPosY);
-			}
-			this.body[len-1].newPosition(this.headX,this.headY);
-		} else {
-			b = new Block(this.headX, this.headY, this.blockSize, this.bodyColour);
-			this.body.push(b);
-			this.growing = this.growing - 1;
-		}
-				
-
-
-		if (this.direction == "WEST") {
-			this.headX = this.headX - this.blockSize*this.speed;
-		}
-		if (this.direction == "NORTH") {
-			this.headY = this.headY - this.blockSize*this.speed;
-		}
-		if (this.direction == "SOUTH") {
-			this.headY = this.headY + this.blockSize*this.speed;
-		}
-		if (this.direction == "EAST") {
-			this.headX = this.headX + this.blockSize*this.speed;
-		}
-	}
-
-	this.changeDirectionLeft = function() {
-		if (this.direction == "NORTH") {
-			this.direction = "WEST";
-			return;
-		}
-		if (this.direction == "SOUTH") {
-			this.direction = "EAST";
-			return;
-		}
-		if (this.direction == "EAST") {
-			this.direction = "NORTH";
-			return;
-		}
-		if (this.direction == "WEST") {
-			this.direction = "SOUTH";
-		}
-	}
-
-	this.changeDirectionRight = function() {
-		if (this.direction == "NORTH") {
-			this.direction = "EAST";
-			return;
-		}
-		if (this.direction == "SOUTH") {
-			this.direction = "WEST";
-			return;
-		}
-		if (this.direction == "EAST") {
-			this.direction = "SOUTH";
-			return;
-		}
-		if (this.direction == "WEST") {
-			this.direction = "NORTH";
-		}
-	}
-
-
-	this.collision = function() {
-		var i;
-
-		if (this.headX < this.blockSize) return true;
-		if (this.headY < this.blockSize) return true;
-		if (this.headX > width - 2*this.blockSize) return true;
-		if (this.headY > height - 2*this.blockSize) return true;
-
-		for (i = 0; i < this.body.length-1; i++) {
-			if ((this.headX == this.body[i].xpos) && (this.headY == this.body[i].ypos)) return true;
-		}
-
-		return false;
-	}
+	return false;
 }
 
 
